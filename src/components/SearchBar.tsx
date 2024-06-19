5fb93169ba3851b1e9b551f18218bf7883891c42
Marko Budal <markobudal@gmail.com>
2024-06-19 18:31:42 +0200

    search view

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+"use client"
+
+import React, { useRef } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
 
 const SearchBar = () => {
-  return (
-    <div className='relative w-full h-14 flex flex-col bg-white '>
-      
-    </div>
-  )
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    
+    return (
+        <div className='relative w-full h-14 flex flex-col bg-white '>
+        <div className='relative h-14 z-10 rounded-md'>
+            <Input 
+                ref={inputRef} 
+                className='absolute inset-0 h-full'
+                onKeyDown={ (e) => {
+                    if(e.key === "Escape") {
+                        inputRef?.current?.blur()
+                    }
+                }}
+            />
+            <Button className='absolute right-0 insert-y-0 h-full rounded-l-none '>
+                <Search className='h-6 w-6'/>
+            </Button>
+        </div>
+        </div>
+    )
 }
 
 export default SearchBar
