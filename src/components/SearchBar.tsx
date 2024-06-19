0e7a3007679f13260975355ab295227d560847fa
Marko Budal <markobudal@gmail.com>
2024-06-19 17:28:09 +0200

    mainview presentation

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className='relative w-full h-14 flex flex-col bg-white '>
+      
+    </div>
+  )
+}
+
+export default SearchBar
